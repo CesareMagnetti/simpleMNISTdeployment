63a0f313605066aaab5bae1c264b8566d57b47bf
cesare <cesare.magnetti98@gmail.com>
2020-12-25 18:49:32 +0100

    finalised app processes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask import Flask, render_template, url_for, request, redirect, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import os
 from torch_utils import transformImage, getPrediction
@@ -7,68 +7,81 @@ app = Flask(__name__)
 
 # set up flags for upload security checks
 app.config['UPLOAD_EXTENSIONS'] = ['jpeg', 'jpg', 'png', 'gif']
-app.config['MAX_CONTENT_LENGTH'] = 1024*1024
+app.config['UPLOAD_PATH'] = os.path.join(os.getcwd(), "uploads")
 
 # get torch utils to transform images and get predictions
 transform_image = transformImage()
 get_prediction = getPrediction()
 
 # handle initial page
-@app.route('/', methods = ['GET','POST'])
+@app.route('/')
 def index():
+    try:
+        files = os.listdir(app.config['UPLOAD_PATH'])
+        return render_template('homepage.html', files = files)
+    except:
+        return render_template('homepage.html') # if no file has been uploaded yet
 
-    if request.method == 'POST':
-        # loop through uploaded files
-        for idx, file_to_upload in enumerate(request.files.getlist('file')):
-            filename = file_to_upload.filename
-            # check file not empty
-            if filename != '':
-                # check file extension
-                if filename.split(".")[-1] not in app.config['UPLOAD_EXTENSIONS']:
-                    return "ERROR: unsupported file extension: {}. "\
-                           "Supported extensions: {}".format(filename.split(".")[-1],
-                                                             app.config['UPLOAD_EXTENSIONS'])
-                # save file to cwd
-                savepath = os.path.join(os.getcwd(), "temp")
-                if not os.path.exists(savepath):
-                    os.mkdir(savepath)
-                file_to_upload.save(os.path.join(savepath, "sample{}.png".format(idx)))
-            # redirect to initial page    
-            return redirect(url_for('index'))
-    else:
-        # render initial page template to not immediately 404
-        return render_template('homepage.html')
+
+# function to upload files
+@app.route('/', methods = ['POST'])
+def upload_files():
+    # loop through uploaded files
+    for file_to_upload in request.files.getlist('file'):
+        filename = file_to_upload.filename
+        # check file not empty
+        if filename != '':
+            # check file extension
+            if filename.split(".")[-1] not in app.config['UPLOAD_EXTENSIONS']:
+                return "ERROR: unsupported file extension: {}. "\
+                        "Supported extensions: {}".format(filename.split(".")[-1],
+                                                            app.config['UPLOAD_EXTENSIONS'])
+
+            # save file(s)
+            if not os.path.exists(app.config['UPLOAD_PATH']):
+                os.mkdir(app.config['UPLOAD_PATH'])
+            file_to_upload.save(os.path.join(app.config['UPLOAD_PATH'],filename))
+  
+    # redirect to initial page    
+    return redirect('/')
+
+# function to retrieve a particular file
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+# remove uploaded file
+@app.route('/delete/<filename>')
+def delete(filename):
+    os.remove(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
 
 # predict on the uploaded files
 @app.route('/predict')
 def predict():
-    # 1 transform uploaded images to tensors
-    tensor_images = []
-    try: 
-        for file in os.listdir("./temp"):
-            with(open("./temp/" + file, 'rb')) as f:
-                tensor_images.append(transform_image(f.read()))
-            os.remove("./temp/" + file) # file not needed to be stored anymore
-            os.rmdir("./temp")
-    except:
-        return "Something went wrong while preprocessing the uploaded images..."
     
-    # 2 get predictions
-    predictions = []
-    try:
-        for image in tensor_images:
-            predictions.append(get_prediction(image))
-    except:
-        return "Something went wrong while making predictions on the uploaded images..."
+    out = []
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    for filename in files:
+        # 1 transform file to tensor
+        try:
+            with(open(os.path.join(app.config['UPLOAD_PATH'],
+                                filename), 'rb')) as f:
+                tensor_image = transform_image(f.read())
+        except:
+            return "Something went wrong while preprocessing image: {}".format(filename)
     
-    # 3 instanciate a dict to return
-    ret = {'images': [], 'class': []}
-    for im, pred in zip(tensor_images, predictions):
-        ret['images'].append(im.detach().cpu().numpy().squeeze())
-        ret['class'].append(pred)
+        # 2 get prediction
+        try:
+            pred = get_prediction(tensor_image)
+        except:
+            return "Something went wrong while making predictions on image: {}".format(filename)
+    
+        # 3 append to out
+        out.append({'filename': filename, 'class': pred})
 
     # 4 return jsonify of the dict
-    return render_template('predict.html', ret=ret)
+    return render_template('predict.html', files=out)
 
 
 if __name__ == "__main__":
